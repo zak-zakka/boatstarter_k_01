070b41f52fa515dd853a489dddb4cee6a71aab43
ruogin <dev@null>
2022-07-01 02:16:36 +0300

    Revert "test commit to github 02 name"
    This reverts commit d81e7e47752db73425702698380277670add47c4.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
-    println("What is your name?")
-    val name = readln()
-    println("Hello, $name")
+    println("Hello Happy World!")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
